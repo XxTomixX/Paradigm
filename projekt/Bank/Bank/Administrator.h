a4d6606047abdf547823229c32f2735952fd51d2
Adam <72575016+adam-glab@users.noreply.github.com>
2021-01-11 13:19:13 +0100

    Implementacja xdxd

--- projekt/Bank/Bank/Administrator.h
+++ projekt/Bank/Bank/Administrator.h
@@ -7,48 +7,42 @@
 class Administrator : Osoba {
 
 private:
+	int id;
 	string haslo;
 	string email;
-	int id;
+	
 
 public:
 	void zaloguj(int id, string haslo);
-
 	void wyloguj();
+	void rejestracja(int id_od_banku, string haslo, string email);
+	bool sprawdz_poprawnosc_danych(string haslo);
+	bool wprowadz_konto_do_bazy(int id, string haslo, string email);
 
 	Konto pobierz_dane_kont();
+	Konto znajdz_konto(int id);
 
 	void zaktualizuj_dane_konta(Konto k);
-
 	void zamroz_konto(Konto k);
-
 	void wyswietl_historie_dzialan(Konto k);
-
 	void modyfikuj_stan_dzialania(Konto k);
 
 	Blad pobierz_liste_bledow();
-
-	void sprawdz_stan_bledu(Blad b);
-
 	Blad zaktualizuj_liste_bledow();
-
-	Konto znajdz_konto(int id);
-
+	void sprawdz_stan_bledu(Blad b);
 	void zmien_status_bledu();
-
+	
 	void monitoruj_lokaty();
-
 	void monitoruj_kredyty();
-
 	void monituruj_przelewy();
 
-	void rejestracja(string id_od_banku, string haslo);
+	void wyswietl_blad(string);
 
-	void odczytaj_dane_administrator();
-
-	void sprawdz_poprawnosc_danych();
-
-	void wyswietl_blad();
+	Administrator(int id, string im, string na, string h, string e) :Osoba(im, na)
+	{
+		haslo = h;
+		email = e;
+	}
 };
 
 #endif
